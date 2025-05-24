e7854ef5fb217ca4ae0303f9471199b352b51976
illiastudihrad1 <illia.studihrad@avisbudget.com>
2025-05-24 12:55:34 +0300

    1.0

--- tests/wiki-page.spec.js
+++ tests/wiki-page.spec.js
@@ -7,4 +7,16 @@ test("Wiki page", async ({ page }) => {
     const searchButton = page.locator('//*[@id="search-form"]/fieldset/button');
     await searchButton.waitFor()
     await page.pause()
+
+    const nativeLanguageButton = page.locator('//*[@id="js-lang-list-button"]');
+    await nativeLanguageButton.waitFor()
+    await page.pause()
+
+    const footerGooglePlayButton = page.locator('//*[@id="www-wikipedia-org"]/footer/div[2]/div/div/ul/li[1]/a/span');
+    await footerGooglePlayButton.waitFor()
+    await page.pause()
+
+    const searchInput = page.locator('//*[@id="searchInput"]');
+    await searchInput.waitFor()
+    await page.pause()
 })
